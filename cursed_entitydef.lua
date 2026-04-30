e1a68611a7b603316f25b81bc80d4f36c0fef84a
Alex Brown <cdsa142@gmail.com>
2026-04-30 01:43:00 -0500

    negative mirror enemies, start of a map

--- cursed_entitydef.lua
+++ cursed_entitydef.lua
@@ -184,6 +184,177 @@ local function addCursedEntities(entitydef)
         end,
     }
 
+    entitydef.mirror_enemy_neg = {
+    compendium_header = "Negative enemies",
+    spr = {
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_slime_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_bat_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_snake_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_scorpion_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_skeleton_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_zombie_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_skeleton_warrior_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_armor_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_warlock_neg.png"),
+        love.graphics.newImage("CursedMod/CursedMod/res/sprite/mirror_enemy_demon_neg.png"),
+    },
+    get_spr = function(ent)
+        if ent == nil then
+            return entitydef.mirror_enemy_neg.spr[1]
+        end
+        if ent.value >= 1000000000 then
+            return entitydef.mirror_enemy_neg.spr[10]
+        end
+        if ent.value >= 100000000 then
+            return entitydef.mirror_enemy_neg.spr[9]
+        end
+        if ent.value >= 10000000 then
+            return entitydef.mirror_enemy_neg.spr[8]
+        end
+        if ent.value >= 1000000 then
+            return entitydef.mirror_enemy_neg.spr[7]
+        end
+        if ent.value >= 100000 then
+            return entitydef.mirror_enemy_neg.spr[6]
+        end
+        if ent.value >= 10000 then
+            return entitydef.mirror_enemy_neg.spr[5]
+        end
+        if ent.value >= 1000 then
+            return entitydef.mirror_enemy_neg.spr[4]
+        end
+        if ent.value >= 100 then
+            return entitydef.mirror_enemy_neg.spr[3]
+        end
+        if ent.value >= 10 then
+            return entitydef.mirror_enemy_neg.spr[2]
+        end
+        return entitydef.mirror_enemy_neg.spr[1]
+    end,
+    can_interact = function(ent, game)
+        if game.player.power > ent.value or game.player.held_item == "vorpal" then
+            return true
+        end
+    end,
+    undo_store = function(ent, game)
+        local out = {
+            battle_gates = {}
+        }
+        local floor_ents = game.level_data[game.player.floor].entities
+        for i=1,#floor_ents do
+            if floor_ents[i].type == "battle_gate" then
+                out.battle_gates[#out.battle_gates+1] = floor_ents[i]
+            end
+        end
+        return out
+    end,
+    interact = function(ent, game)
+        game.unlocks:set_unlock_flag("compendium_flag_enemy")
+        if game.level_data.metadata.computed_flags.money_system then
+            game.unlocks:set_unlock_flag("compendium_flag_money")
+        end
+        if game.player.held_item == "vorpal" then
+            log("vorpal sword consumed")
+            local a = game:spawn_anim("kill_vorpal",ent.x*16-12,ent.y*16-12)
+            g_sfx:play("hit_vorpal")
+            a.params = {
+                dir_x = ent.x - game.player.x,
+                dir_y = ent.y - game.player.y,
+            }
+            game.player.held_item = nil
+        elseif game.player.held_item == "light_rod" then
+            log("light rod consumed")
+            game:spawn_anim("kill_lrod",ent.x*16-12,ent.y*16-12)
+            g_sfx:play("hit_rod")
+            game.player.held_item = nil
+            game:modify_player_power(ent.value)
+        elseif game.player.held_item == "shield" then
+            game:modify_player_power(-math.floor(ent.value/2))
+            local a = game:spawn_anim("kill_normal",ent.x*16-12,ent.y*16-12)
+            g_sfx:play("hit")
+            a.params = {
+                dir_x = ent.x - game.player.x,
+                dir_y = ent.y - game.player.y,
+            }
+        elseif game.player.held_item == "keysmasher" then
+            if game.flags.negative_keys then
+                game:modify_player_power(-ent.value + game.player.keys * game.player.keys)
+            else
+                game:modify_player_power(-ent.value + game.player.keys * game.player.dark_keys)
+            end
+            local a = game:spawn_anim("kill_normal",ent.x*16-12,ent.y*16-12)
+            g_sfx:play("hit")
+            a.params = {
+                dir_x = ent.x - game.player.x,
+                dir_y = ent.y - game.player.y,
+            }
+        elseif game.player.held_item == "rapier" then
+            local tier = 1
+            local val = ent.value
+            while val>=10 and tier<=10 do
+                tier = tier + 1
+                val = val /10
+            end
+            game:modify_player_power(-ent.value + game.player.total_crowns * tier)
+            local a = game:spawn_anim("kill_normal",ent.x*16-12,ent.y*16-12)
+            g_sfx:play("hit")
+            a.params = {
+                dir_x = ent.x - game.player.x,
+                dir_y = ent.y - game.player.y,
+            }
+        else
+            game:modify_player_power(-ent.value)
+            local a = game:spawn_anim("kill_normal",ent.x*16-12,ent.y*16-12)
+            g_sfx:play("hit")
+            a.params = {
+                dir_x = ent.x - game.player.x,
+                dir_y = ent.y - game.player.y,
+            }
+        end
+        local val_tmp = ent.value
+        local money_gain = 1
+        while val_tmp >= 10 and money_gain < 10 do
+            val_tmp = val_tmp / 10
+            money_gain = money_gain + 1
+        end
+        if game.player.held_item == "golden_dagger" then
+            money_gain = money_gain + 2
+        elseif game.player.held_item == "golden_claymore" then
+            money_gain = money_gain * 2
+        end
+        game.player.gold = game.player.gold + money_gain
+        ent.type = nil
+        --battle gates
+        local floor_ents = game.level_data[game.player.floor].entities
+        local play_door_sound = false
+        for i=1,#floor_ents do
+            if floor_ents[i].type == "battle_gate" then
+                floor_ents[i].value = floor_ents[i].value - 1
+                floor_ents[i].value_str = ""..floor_ents[i].value
+                if floor_ents[i].value == 0 then
+                    log("battle gate at "..(floor_ents[i].x)..","..(floor_ents[i].y).." opened")
+                    play_door_sound = true
+                    game:spawn_anim("open",floor_ents[i].x*16-12,floor_ents[i].y*16-12)
+                    floor_ents[i].type = nil
+                end
+                game.unlocks:set_unlock_flag("compendium_flag_bgate")
+            end
+        end
+        if play_door_sound then
+            g_sfx:play("key_door")
+        end
+        return true
+    end,
+    undo_perform = function(ent, game, extra_data)
+        ent.type = "enemy_neg"
+        for i=1,#extra_data.battle_gates do
+            extra_data.battle_gates[i].type = "battle_gate"
+            extra_data.battle_gates[i].value = extra_data.battle_gates[i].value + 1
+            extra_data.battle_gates[i].value_str = ""..extra_data.battle_gates[i].value
+        end
+    end,
+}
+
     entitydef.curse_weaken = {
         compendium_header = "Elixirs",
         particle = util.standard_particle_spawner,
